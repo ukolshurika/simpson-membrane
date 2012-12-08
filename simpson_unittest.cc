838a5035181275f151add702ed30719988ec927f
Alexandra Ukolova <aukolova@at-consulting.ru>
2012-12-08 22:45:18 +0400

    Add simpson test

--- simpson_unittest.cc
+++ simpson_unittest.cc
@@ -18,6 +18,36 @@ struct ConstantFunction {
   }
 };
 
+struct LinearFunction {
+  double operator () (double x) const {
+    return x;
+  }
+};
+
+struct SinusFunction {
+  double operator () (double x) const {
+    return sin(x);
+  }
+};
+
+struct InverseSqrtFunction {
+  double operator () (double x) const {
+    return 1/sqrt(x);
+  }
+};
+
 TEST(SimpsonTest, Constant) {
   ASSERT_TRUE(eq(0.0, Simpson::Integrate(0, 1, kNumSteps, ConstantFunction())));
+}
+
+TEST(SimpsonTest, Linear) {
+  ASSERT_TRUE(eq(0.5, Simpson::Integrate(0, 1, kNumSteps, LinearFunction())));
+}
+
+TEST(SimpsonTest, Sinus) {
+  ASSERT_TRUE(eq(2, Simpson::Integrate(0, M_PI, kNumSteps, SinusFunction())));
+}
+
+TEST(SimpsonTest, InverseSqrt) {
+  ASSERT_TRUE(eq(2.0, Simpson::Integrate(1, 4, kNumSteps, InverseSqrtFunction())));
 }
