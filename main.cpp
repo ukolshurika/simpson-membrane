7a7bff56894c628ec2957f30de0f8b9e0c6cf239
Alexandra Ukolova <ukolshurika@gmail.com>
2012-12-17 23:21:07 +0400

    use std clock

--- main.cpp
+++ main.cpp
@@ -10,18 +10,18 @@ int main(){
   double q = 2650.0/88.3/1000000;
   double n = 3.4;
   
-  timespec start, stop;
+  std::clock_t start;
   int steps;
   cin >> steps;
 
 
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+  start = clock();
   
   Membrane m(h0, q, n, 0.1, 999, steps);
   m.IntegrateForAnimation(steps);
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
 
-  cout << ((stop.tv_nsec)/100000000.0 - (start.tv_nsec)/100000000.0) << endl;
+  cout << (( std::clock() - start ) / (double) CLOCKS_PER_SEC) << endl;
   // m.OutputResult();
   return 0;
 }
