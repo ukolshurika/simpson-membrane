0dd85ac6c5dda5ab027819b30406519445c9742e
Alexandra Ukolova <ukolshurika@gmail.com>
2013-05-02 19:32:19 +0400

    add gaus method

---
+++ src/gaus.h
@@ -0,0 +1,22 @@
+#ifndef SIMPSON_H_
+#define SIMPSON_H_
+
+#include <cmath>
+
+class Gaus{
+  public:
+
+  template <class F>
+  static double Integrate(double from, double to, const F& f){
+    double summand = (from+to)/2.0, sum;
+    sum =  128/255.0*f(summand)+
+           (322+13*sqrt(70))/900.0*f(summand+1/3.0*sqrt(5-2*sqrt(10/7.0))*(to-from)/2.0) +
+           (322+13*sqrt(70))/900.0*f(summand-1/3.0*sqrt(5-2*sqrt(10/7.0))*(to-from)/2.0) +
+           (322-13*sqrt(70))/900.0*f(summand+1/3.0*sqrt(5+2*sqrt(10/7.0))*(to-from)/2.0) +
+           (322-13*sqrt(70))/900.0*f(summand-1/3.0*sqrt(5+2*sqrt(10/7.0))*(to-from)/2.0);
+
+    return sum*(to-from)/2.0;
+  }
+};
+
+#endif  // SIMPSON_H_
