044b631b1aa99998bdfb5e79787379ce73f7fa65
Alexandra Ukolova <ukolshurika@gmail.com>
2013-05-18 16:44:05 +0400

    paper stuff

--- src/gaus.h
+++ src/gaus.h
@@ -8,12 +8,12 @@ class Gaus{
 
   template <class F>
   static double Integrate(double from, double to, const F& f){
-    double summand = (from+to)/2.0, sum;
+    double summand = (from+to)/2.0, sum, multiplier = (to-from)/2.0;
     sum =  128/255.0*f(summand)+
-           (322+13*sqrt(70))/900.0*f(summand+1/3.0*sqrt(5-2*sqrt(10/7.0))*(to-from)/2.0) +
-           (322+13*sqrt(70))/900.0*f(summand-1/3.0*sqrt(5-2*sqrt(10/7.0))*(to-from)/2.0) +
-           (322-13*sqrt(70))/900.0*f(summand+1/3.0*sqrt(5+2*sqrt(10/7.0))*(to-from)/2.0) +
-           (322-13*sqrt(70))/900.0*f(summand-1/3.0*sqrt(5+2*sqrt(10/7.0))*(to-from)/2.0);
+           (322+13*sqrt(70))/900.0*f(summand+1/3.0*sqrt(5-2*sqrt(10/7.0))*multiplier +
+           (322+13*sqrt(70))/900.0*f(summand-1/3.0*sqrt(5-2*sqrt(10/7.0))*multiplier +
+           (322-13*sqrt(70))/900.0*f(summand+1/3.0*sqrt(5+2*sqrt(10/7.0))*multiplier +
+           (322-13*sqrt(70))/900.0*f(summand-1/3.0*sqrt(5+2*sqrt(10/7.0))*multiplier;
 
     return sum*(to-from)/2.0;
   }
